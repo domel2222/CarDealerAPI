d47e6d33298ac076330a34dd8fc8c00fb16268a6
Dominik Wikliński <domel2222@gmail.com>
2021-03-11 10:35:13 +0100

    created models car address dealer

---
+++ Models/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerAPI.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string NameMark { get; set; }
+        public string Model { get; set; }
+        
+        public decimal Price { get; set; }
+        public int DealerId { get; set; }
+        public virtual Dealer Dealr { get; set; }
+
+    }
+}
---
+++ Models/Dealer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerAPI.Models
+{
+    public class Dealer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string DealerName { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool TestDrive { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactNumber { get; set; }
+        public int AddressId { get; set; }
+        public virtual Address Address { get; set; }
+
+        public virtual List<Car> Cars { get; set; }
+    }
+}
