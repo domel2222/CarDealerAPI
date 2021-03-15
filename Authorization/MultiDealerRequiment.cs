6e0cdd6e5e266ec8dee45b9ca38a2e72e8ae7d86
Dominik Wikliński <domel2222@gmail.com>
2021-03-15 08:21:12 +0100

    created mutileDealerrequirment
    used  AuthorizationHandler
    and overide HandleRequirementAsync method

---
+++ Authorization/MultiDealerRequiment.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerAPI.Authorization
+{
+    public class MultiDealerRequiment : IAuthorizationRequirement
+    {
+        public MultiDealerRequiment(int minDealer)
+        {
+            MinDealer = minDealer;
+        }
+
+        public int MinDealer { get; }
+    }
+}
---
+++ Authorization/MultiDealerRequimentHandler.cs
@@ -0,0 +1,36 @@
+﻿using CarDealerAPI.Contexts;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerAPI.Authorization
+{
+    public class MultiDealerRequimentHandler : AuthorizationHandler<MultiDealerRequiment>
+    {
+        private readonly DealerDbContext _dealerDbContext;
+
+        public MultiDealerRequimentHandler(DealerDbContext dealerDbContext)
+        {
+            this._dealerDbContext = dealerDbContext;
+        }
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MultiDealerRequiment requirement)
+        {
+            var userId =   int.Parse(context.User.FindFirst(u => u.Type == ClaimTypes.NameIdentifier).Value);
+
+            var dealerCount = _dealerDbContext
+                .Dealers
+                .Count(d => d.CreatedById == userId);
+
+            if (dealerCount >= requirement.MinDealer)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
