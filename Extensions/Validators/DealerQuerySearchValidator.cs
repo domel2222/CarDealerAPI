49cd9e4cfece7698fcb3ff111c043cb0f1ba0e36
Dominik Wikliński <domel2222@gmail.com>
2021-03-16 08:52:20 +0100

    add rule in Validator for Sort

--- Extensions/Validators/DealerQuerySearchValidator.cs
+++ Extensions/Validators/DealerQuerySearchValidator.cs
@@ -1,4 +1,5 @@
 ﻿using CarDealerAPI.Extensions;
+using CarDealerAPI.Models;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,14 @@ namespace CarDealerAPI.Extensions.Validators
     public class DealerQuerySearchValidator : AbstractValidator<DealerQuerySearch>
     {
         private int[] possibleChose = new[] { 5, 10, 15, 20 };
+
+        private string[] possibleColumnSort =
+            {
+            nameof(Dealer.DealerName),
+            nameof(Dealer.Description),
+            nameof(Dealer.Category),
+            nameof(Dealer.Address.Country)
+            };
         public DealerQuerySearchValidator()
         {
             RuleFor(p => p.PageNumber).GreaterThanOrEqualTo(1);
@@ -23,6 +32,10 @@ namespace CarDealerAPI.Extensions.Validators
                      context.AddFailure("Pagesize :", $"Page size must be between {string.Join(",", possibleChose)}");
                  }
              });
+
+            RuleFor(r => r.SortBy)
+                .Must(v => string.IsNullOrEmpty(v) || possibleColumnSort.Contains(v))
+                .WithMessage($"Sort must be on the range {string.Join(",", possibleColumnSort)}");
         }
     }
 }
